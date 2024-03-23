a62e66b9a001834a2df1f1263a769e89d718f285
mlum-555 <153239613+mlum-555@users.noreply.github.com>
2024-03-23 03:29:13 -0700

    Makobii doesn't use navmesh, outdoor3 messed with.
    Poked around terrain settings with outdoor 3 and found / tried a few things; thought a tighter level with a slope worked better, though new trees / etc need to be put in. Makobii now simply translates towards target position, though movement needs tweaking. Toyen/makobii prefabs updated in turn with this. Character folder reorganized for clarity.

--- Assets/Scripts/MakobiiScript.cs
+++ Assets/Scripts/MakobiiScript.cs
@@ -6,30 +6,93 @@ using UnityEngine;
 public class MakobiiScript : MonoBehaviour
 {
 
-    public GameObject destination;
-    public UnityEngine.AI.NavMeshAgent navAgent;
+    GameObject destination;
+    //public UnityEngine.AI.NavMeshAgent navAgent;
     // Start is called before the first frame update
 
     public Animator makAnimator; //this should ideally be moved somewhere else
+
+    public float moveSpeed;
+
+    public float accelRate, maxAccel;
+
+    float currAccel;
+
+    float currMoveSpeed;
+
+    bool moving = false;
+
+    public float decelStart;
+    //maybe just get this from initial distance?  when it starts movin
+    //just take movement speed from current distance, scale it by that
+
+    GameObject player;
+
+    GameObject lookTarg;
+    
     void Start()
     {
-        navAgent.SetDestination(destination.transform.position);
+        //player = GameObject.FindFirstObjectByType<PlayerController>().gameObject;
+        lookTarg = GameObject.Find("MakLookTarg").gameObject;
+        destination = GameObject.Find("MakobiiDest").gameObject;
+        //navAgent.SetDestination(destination.transform.position);
     }
 
     // Update is called once per frame
     void Update()
     {
-        navAgent.SetDestination(destination.transform.position);
-        if (navAgent.velocity.magnitude > 0)
+      //  navAgent.SetDestination(destination.transform.position);
+        if (transform.position!=destination.transform.position)
         {
+            //wait nvm uh
             makAnimator.SetBool("Moving", true);
+            moving = true;
         }
-        else makAnimator.SetBool("Moving", false);
+        else
+        {
+            makAnimator.SetBool("Moving", false);
+            moving = false;
+
+
+            if (currAccel > 0) currAccel -= (accelRate * Time.deltaTime);
+
+        }
+
+        if(moving && currAccel < maxAccel)
+        {
+            currAccel += (accelRate * Time.deltaTime);
+        }
+
+        if (moving && currMoveSpeed < moveSpeed)
+        {
+            currMoveSpeed += currAccel;
+        }
+
+        //uhhh square scale? idk
+        //just basically movement speed gets lower as it gets closer; inverse proportional
+
+        //accel = uhh
+
+        //current move speed should still be the final thing
+        //you want to be scaling move speed
+
+        //do it on a 1 scale instead
+
+        currMoveSpeed = Vector3.Distance(transform.position, destination.transform.position) * moveSpeed;
+
+        transform.position = Vector3.MoveTowards(transform.position, destination.transform.position, currMoveSpeed*Time.deltaTime);
+
+        float tempY = transform.rotation.y;
+        transform.LookAt(lookTarg.transform.position);
+        
     }
 
     [ContextMenu("setDest")]
     void setDest()
     {
-        navAgent.SetDestination(destination.transform.position);
+       // navAgent.SetDestination(destination.transform.position);
     }
+    //ok just do moving stuff manually
+
+
 }
