214ea8af4fe386933b950edb22916333aa5203e0
wall-e785 <116865383+wall-e785@users.noreply.github.com>
2024-04-04 21:57:24 -0700

    scene progression updated + fade transition improved

--- Assets/Scripts/FadeTransition.cs
+++ Assets/Scripts/FadeTransition.cs
@@ -22,38 +22,70 @@ public class FadeTransition : MonoBehaviour
         }
         else
         {
+            SceneManager.sceneLoaded += OnSceneLoaded;
+
             yield return new WaitForSeconds(1);
             switch (SceneManager.GetActiveScene().name)
             {
                 case "MainMenu":
-                    SceneManager.LoadScene("DreamTutorial");
+                    SceneManager.LoadScene("DreamTutorial"); //switch to cutscene #1
                     break;
+                /*case "MainMenu": //cutscene1
+                    SceneManager.LoadScene("DreamTutorial"); 
+                    break;*/
                 case "DreamTutorial":
-                    SceneManager.LoadScene("OutsideLevelZero");
+                    SceneManager.LoadScene("OutsideLevelZero"); //switch to cutscene #2
                     break;
+                /*case "DreamTutorial": //cutscene2
+                    SceneManager.LoadScene("OutsideLevelZero"); //switch to village
+                    break;*/
                 case "OutsideLevelZero":
-                    SceneManager.LoadScene("Act1Scene3");
+                    SceneManager.LoadScene("Act1Scene3"); //makobii intro cutscene
                     break;
-                case "Act1Scene3":
-                    SceneManager.LoadScene("OutsideLevel1");
+                case "Act1Scene3": //makobii intro cutscene
+                    SceneManager.LoadScene("OutsideLevel1"); //walkingone
                     break;
-                case "OutsideLevel1":
-                    SceneManager.LoadScene("Act2Scene2");
+                case "OutsideLevel1": //walking one
+                    SceneManager.LoadScene("Act2Scene2");//dream 1
                     ChoiceManager.shardsFound++;
                     break;
-                case "Act2Scene2":
-                    SceneManager.LoadScene("OutsideLevel2");
+                case "Act2Scene2": //family picnic dream
+                    SceneManager.LoadScene("OutsideLevel2"); //walking two
                     break;
-                case "OutsideLevel2":
-                    SceneManager.LoadScene("DreamLevelOne");
+                case "OutsideLevel2": //walkingtwo
+                    SceneManager.LoadScene("DreamLevelOne"); //dream 2 intro cutscene
                     ChoiceManager.shardsFound++;
                     break;
-                case "DreamLevelOne":
-                    SceneManager.LoadScene("DreamLevelOneEnd");            
+                /*case "OutsideLevel2": //dream 2 intro cutscene
+                    SceneManager.LoadScene("DreamLevelOne"); //dream 2
+                    ChoiceManager.shardsFound++;
                     break;
-            }     
-            StartCoroutine(fadeOut());
+                case "OutsideLevel2": //dream 2
+                    SceneManager.LoadScene("DreamLevelOne"); //dream 2 end cutscene
+                    ChoiceManager.shardsFound++;
+                    break;*/
+                case "DreamLevelOne": //dream 3 intro cutscene
+                    SceneManager.LoadScene("DreamLevelOneEnd"); //dream 3
+                    break;
+                /*case "DreamLevelOne": //dream 3
+                    SceneManager.LoadScene("DreamLevelOneEnd"); //dream 3 end cutscene 
+                    break;
+                 case "DreamLevelOne": //dream 3 end
+                    SceneManager.LoadScene("DreamLevelOneEnd"); dream 4      
+                    break;
+                case "DreamLevelOne": //dream 4
+                    SceneManager.LoadScene("DreamLevelOneEnd"); //dream 4 end cutscene
+                    break;
+                case "DreamLevelOne": //dream 4 end cutscene
+                    SceneManager.LoadScene("DreamLevelOneEnd"); //finale cutscene
+                    break; */
+            }          
         }
+    }
+
+    void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        StartCoroutine(fadeOut());
     }
 
     IEnumerator fadeOut()
@@ -67,6 +99,7 @@ public class FadeTransition : MonoBehaviour
         else
         {
             StopAllCoroutines();
+            SceneManager.sceneLoaded -= OnSceneLoaded;
         }
     }
 
